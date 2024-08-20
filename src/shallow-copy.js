da2b9d95cb4c62951a03a61128625b7d738d679e
Felipe Ortiz <felort15@gmail.com>
2024-08-19 22:37:02 -0600

    recursive

---
+++ src/shallow-copy.js
@@ -0,0 +1,42 @@
+/*const obj1 = {
+    a: "a",
+    b: "b",
+    c: {
+      d: "d",
+      e: "e",
+    },
+    editA() {
+      this.a = "AAAAAA";
+    }
+  };
+  
+  const stringifiedComplexObj = JSON.stringify(obj1);
+  const obj2 = JSON.parse(stringifiedComplexObj);*/
+
+  // function recursiva() {
+//   if (/* validacion */) {
+//     // llamados recursivos
+//   } else {
+//     // llamados normales, sin recursividad
+//   }
+// }
+
+const numeritos = [0,1,2,3,4,5,5,6,7,8,9,435678,7,2,3];
+// let numerito = 0;
+// for (let index = 0; index < numeritos.length; index++) {
+//   numerito = numeritos[index];
+//   console.log({ index, numerito });
+// }
+
+function recursiva(numbersArray) {
+  if (numbersArray.length === 0) {
+    return; // explicitly return from the base case
+  }
+
+  const firstNum = numbersArray[0];
+  console.log(firstNum);
+
+  // create a new array without the first element
+  const rest = numbersArray.slice(1);
+  recursiva(rest);
+}
